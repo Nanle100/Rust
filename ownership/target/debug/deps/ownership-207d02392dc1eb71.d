4c6bce50a4ac69a27a7a8acd3a395394864154a5
Nathaniel Nanle <nathanielnanle100@gmail.com>
2024-10-18 17:52:35 +0100

    ownership with rust

---
+++ ownership/target/debug/deps/ownership-207d02392dc1eb71.d
@@ -0,0 +1,5 @@
+/home/ga/Desktop/Rust/ownership/target/debug/deps/ownership-207d02392dc1eb71: src/main.rs
+
+/home/ga/Desktop/Rust/ownership/target/debug/deps/ownership-207d02392dc1eb71.d: src/main.rs
+
+src/main.rs:
---
+++ ownership/target/debug/ownership.d
@@ -0,0 +1,1 @@
+/home/ga/Desktop/Rust/ownership/target/debug/ownership: /home/ga/Desktop/Rust/ownership/src/main.rs
